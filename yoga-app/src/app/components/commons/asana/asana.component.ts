d55c1f2edbe47eca7293cc621304a05f4743b177
Juan Ortega <91577323+Full-Juan-Ortega@users.noreply.github.com>
2023-08-02 15:12:03 -0300

    install material + asana component + add service

--- yoga-app/src/app/components/commons/asana/asana.component.ts
+++ yoga-app/src/app/components/commons/asana/asana.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/core/http/api.service';
 
 @Component({
   selector: 'app-asana',
   templateUrl: './asana.component.html',
-  styleUrls: ['./asana.component.scss']
+  styleUrls: ['./asana.component.scss'],
 })
 export class AsanaComponent {
+  data: any = 0;
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.getDataFromApi();
+  }
+
+  getDataFromApi(): void {
+    this.apiService.getData().subscribe({
+      next: (response) => {
+        this.data = response;
+        console.log('trayendo : ', response);
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      },
+    });
+  }
 }
