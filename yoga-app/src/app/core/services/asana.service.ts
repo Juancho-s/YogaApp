a5cc5267b1ded863f7764561873b5be3b63d9c07
Juan Ortega <91577323+Full-Juan-Ortega@users.noreply.github.com>
2023-08-04 00:02:01 -0300

    add endpoint and pose model , fix handle error lenguage to english , and add improvements to asanaService y asana component.

--- yoga-app/src/app/core/services/asana.service.ts
+++ yoga-app/src/app/core/services/asana.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, retry, throwError } from 'rxjs';
+import { ApiEndpoints } from 'src/app/models/apiEndPoints';
+import { Pose } from 'src/app/models/pose';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AsanaService {
+export class ApiService {
+  apiEndpoints: ApiEndpoints;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.apiEndpoints = new ApiEndpoints();
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error(`An error has ocurred : ${error.error}`);
+    } else {
+      console.error(
+        `BackEnd error: ${error.status}. Error info: ${error.error}`
+      );
+    }
+    return throwError(() => new Error('Error in the random Category petition'));
+  }
+
+  getData(): Observable<Pose> {
+    return this.http
+      .get<Pose>(this.apiEndpoints['pose-by-id'] + '1')
+      .pipe(retry(2), catchError(this.handleError));
+  }
 }
--- yoga-app/src/app/core/services/category.service.ts
+++ yoga-app/src/app/core/services/category.service.ts
@@ -4,29 +4,27 @@ import { Observable, catchError, retry, throwError } from 'rxjs';
 import { Category } from 'src/app/models/category';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  private apiUrl = 'https://yoga-api-nzy4.onrender.com/v1/categories';
 
-  private apiUrl ="https://yoga-api-nzy4.onrender.com/v1/categories";
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  handleError(error: HttpErrorResponse){
-    if(error.status === 0){
-      console.error(`Ha ocurrido un error: ${error.error}`);
-    }else{
-      console.error(`Error en el backend: ${error.status}. El error es: ${error.error}`);
+  handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error(`An error has ocurred : ${error.error}`);
+    } else {
+      console.error(
+        `BackEnd error: ${error.status}. Error info: ${error.error}`
+      );
     }
-    return throwError(
-      ()=> new Error('Error en la petición de contacto aleatorio')
-    );
+    return throwError(() => new Error('Error in the random Category petition'));
   }
 
-  getAllCategories():Observable<Category[]>{
-    
+  getAllCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(this.apiUrl).pipe(
-      retry(2),  //numero de intento de peticiones, sino sale se envia el error
+      retry(2), //numero de intento de peticiones, sino sale se envia el error
       catchError(this.handleError)
       // hay q agregar la posibilidad de recibir un error al suscribirse desde el componente q llame al servicio
     );
